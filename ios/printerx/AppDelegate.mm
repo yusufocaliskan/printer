2ff8c5e713d8b3831826f41ecc18a27f643a65b5
Yusuf Çalışkan <yusufocaliskan@gmail.com>
2023-09-29 20:47:45 +0300

    Icon changed

--- ios/printerx/AppDelegate.mm
+++ ios/printerx/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"bluetoothPrinter", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"printerx", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
